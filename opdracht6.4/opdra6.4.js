7ddfb753da271e3a80afb4a20e169869deb3b5af
Josevnnoord <114076703+Josevnnoord@users.noreply.github.com>
2022-10-25 16:43:36 +0200

    Add files via upload

---
+++ opdracht6.4/opdra6.4.js
@@ -0,0 +1,23 @@
+function calculate(num, num2) {
+    let som = num + num2;
+    console.log(som);
+}
+calculate(6, 16);
+
+
+function multiplyByFive(getal1, getal2) {
+    let som = getal1 * getal2;
+    console.log (som);
+
+}
+multiplyByFive (19, 69);
+
+
+function calculateMinutes() {
+    let getal1  = prompt ("Schrijf een nummer hoger dan 60");
+    let getal2  = 60
+    alert (getal1 / getal2);
+    console.log(getal1 / getal2);
+
+}
+calculateMinutes();
