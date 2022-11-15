901d1500840e6e0c00e77329c75ae29c8aec011a
Josevnnoord <114076703+Josevnnoord@users.noreply.github.com>
2022-11-15 13:46:04 +0100

    Add files via upload

---
+++ opdracht1.php
@@ -0,0 +1,9 @@
+<?php
+echo "<h1>Hallo,</h1>";
+echo "Ik zit op het ";
+$schoolnaam = "Techniek College Rotterdam !";
+echo "$schoolnaam";
+
+echo "<p> ik doe mbo niveau 4";
+$niveau = "mbo niveau 4";
+echo "$niveau";
